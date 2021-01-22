ff831a233bebc74a303eecfb2ed3e9f358f39bba
xjayno <Jannik.otn@gmail.com>
2021-01-22 16:37:51 +0100

    HTMLCalculator
    Revised Styling, Revised JavaScript (Just Eventlistener not Queryselectorall)

--- HTMLCalculator/js/rechner.js
+++ HTMLCalculator/js/rechner.js
@@ -2,8 +2,11 @@
     window.onload = function () {
         //Saving operator
         let operation = null;
+
+        let value = null;
         //Boolean to clear input after Calculation has been done
         let isCalculated = false;
+
 
         let root = document.getElementById("taschenrechner");
         let display = root.querySelector(".display");
@@ -13,99 +16,85 @@
         let input = display.querySelector('.display');
         let equationvalue = equation.querySelector('.equation');
 
-
-        //numbers
-        keys.querySelectorAll('.number.btn').forEach(element => {
-            element.onclick = function () {
+        keys.addEventListener("click", function (event) {
+            if (event.target.classList.contains('number')) {
                 if (isCalculated === true) {
                     input.value = null;
                     isCalculated = false;
                 }
-                input.value += element.value;
+                input.value += event.target.value;
             }
-        });
-
-        //Operator
-        keys.querySelectorAll('.operation.btn').forEach(element => {
-            element.onclick = function () {
-                operation = element.value;
-                equationvalue.value = input.value + operation;
+            if (event.target.classList.contains('operation')) {
+                operation = event.target.value;
+                value = parseFloat(input.value);
+                equationvalue.value = value + operation;
                 input.value = null;
             }
-        });
-
-        //dot
-        keys.querySelectorAll('.dot.btn').forEach(element => {
-            element.onclick = function () {
+            if (event.target.classList.contains('dot')) {
                 if (input.value) {
                     if (!input.value.includes('.')) {
-                        input.value += element.value;
+                        input.value += event.target.value;
                     }
                 } else {
-                    input.value += '0' + element.value;
+                    input.value += '0' + event.target.value;
                 }
             }
-        });
-
-        //C
-        keys.querySelectorAll('.c.btn').forEach(element => {
-            element.onclick = function () {
-                input.value = null;
-                equationvalue.value = null;
-            }
-        });
-
-        //CE
-        keys.querySelectorAll('.ce.btn').forEach(element => {
-            element.onclick = function () {
-                input.value = null;
-            }
-        });
-
-        //del
-        keys.querySelectorAll('.del.btn').forEach(element => {
-            element.onclick = function () {
-                input.value = input.value.substr(0, input.value.length - 1);
-            }
-        });
-
-
-        //equals
-        keys.querySelectorAll('.equals.btn').forEach(element => {
-            element.onclick = function () {
-                let firstvalue = parseFloat(equationvalue.value.substr(0, equationvalue.value.length - 1));
-                let secondvalue = parseFloat(input.value);
-                let value = null;
-                switch (operation) {
-                    case "+":
-                        value = firstvalue + secondvalue;
-                        break;
-                    case "-":
-                        value = firstvalue - secondvalue;
-                        break;
-                    case "/":
-                        value = firstvalue / secondvalue;
-                        break;
-                    case "x":
-                        value = firstvalue * secondvalue;
-                        break;
-                    default:
-                        input.value = secondvalue;
-                        break;
+            if (event.target.classList.contains('c')) {
+                if (event.target.value === 'c') {
+                    input.value = null;
+                    equationvalue.value = null;
+                    operation = null;
+                    value = null;
+                } else {
+                    input.value = null;
                 }
+            }
+            if (event.target.classList.contains('del')) {
+                input.value = input.value.substr(0, input.value.length);
+            }
+            if (event.target.classList.contains('equals')) {
                 if (value) {
-                    isCalculated = true;
-                    if (isOdd(value) === 0) {
-                        input.value = value;
-                    } else {
-                        input.value = value.toFixed(2);
+                    let result;
+                    switch (operation) {
+                        case "+":
+                            result = value + parseFloat(input.value);
+                            break;
+                        case "-":
+                            result = value - parseFloat(input.value);
+                            break;
+                        case "/":
+                            result = value / parseFloat(input.value);
+                            break;
+                        case "x":
+                            result = value * parseFloat(input.value);
+                            break;
+                        default:
+                            result = parseFloat(input.value);
                     }
+                    if (isOdd(result) === 0) {
+                        input.value = result;
+                    } else {
+                        input.value = result.toFixed(2);
+                    }
+                    equationvalue.value = null;
                 }
-                equationvalue.value = null;
             }
-        });
+        }, false);
+
     };
+
+    //returns 0 if even, 1 if odd
     function isOdd(num) {
         return num % 2;
+    }
+
+    function addNumber(input, num, bool) {
+        if (bool === true) {
+            input.value = null;
+            input.value += num.value;
+            bool = false;
+        } else {
+            input.value += num.value;
+        }
     }
 }())
